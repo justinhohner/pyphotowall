cdf9e3d18d818c24ac893f8d2efe84fa85db16f1
justin hohner <justin.hohner@gmail.com>
2021-10-06 10:49:34 -0500

    move file management to sqlite

--- photowall.py
+++ photowall.py
@@ -1,43 +1,72 @@
 import os
 from flask import Flask, render_template, jsonify, send_file, send_from_directory
-from wall.photos import findPhotos
+from flask_apscheduler import APScheduler
+import sqlite3
+from wall.photos import importPhotos
 import json
 import io
 from PIL import Image
+from flask import g
 
-app = Flask(__name__)
+DATABASE = 'photowall.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+class Config:
+    SCHEDULER_API_ENABLED = True
+
 IMGROOT = "imgs"
 MaxWidth = 1920
 MaxHeight = 1080
+
+app = Flask(__name__)
+app.config.from_object(Config())
+scheduler = APScheduler()
+
+scheduler.init_app(app)
+scheduler.start()
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+@scheduler.task('interval', id='imageImport', seconds=900, misfire_grace_time=900)
+def imageImport():
+    print('Job 1 executed')
+    #importPhotos(IMGROOT)
+
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 @app.route("/imgs/<imgname>")
 def imghandle(imgname):
-    photo = next(item for item in app.photos if item["name"] == imgname)
-    """if (photo["width"] <= MaxWidth) and (photo["height"] <= MaxHeight):
-        return send_file(photo["fname"], mimetype=photo["mime"])
-    else:
-        parts = photo["fname"].rsplit('.', 1)
-        fname = parts[0] + ".png"
-        with Image.open(photo["fname"]) as img:
-            new_width = MaxWidth
-            new_height = MaxHeight
-            if img.width < MaxWidth:
-                new_width = img.width
-            if img.height < MaxHeight:
-                new_height = img.height
-            imgIO = io.BytesIO()
-            img = img.resize((new_width, new_height), resample=Image.LANCZOS, box=(0,0,MaxWidth, MaxHeight))
-            img.save(imgIO, "PNG")
-            imgIO.seek(0)
-            return send_file(imgIO, mimetype='image/png', as_attachment=True, download_name=fname)"""
-    return send_file(photo["fname"], mimetype=photo["mime"])
+    cur = get_db().cursor()
+    cur.execute("select fname, mime from photowall where name=:name", {"name": imgname})
+    #photo = next(item for item in app.photos if item["name"] == imgname)
+    photo = cur.fetchone()
+    print(photo)
+    return send_file(photo[0], mimetype=photo[1])
 
 @app.route("/api/wall.json")
 def wallApi():
-    return jsonify(app.photos)
+    cur = get_db().cursor()
+    cur.execute("select name, width, height, mime from photowall")
+    resp = cur.fetchall()
+    photos = []
+    for img in resp:
+        photos.append({"name": img[0],
+                       "width": img[1],
+                       "height": img[2],
+                       "mime": img[3]
+        })
+    return jsonify(photos)
 
 @app.route("/slide")
 def slide():
@@ -49,5 +78,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.photos = findPhotos(IMGROOT)
     app.run(debug = True)
--- wall/photos.py
+++ wall/photos.py
@@ -1,13 +1,50 @@
 import os
 from datetime import datetime
 from PIL import Image
-imname = 'imgs/IMG_20141105_212028308.jpg'
+import sqlite3
 
 supported_files = ['jpg', 'png']
 
-#"name":"100_0002.JPG","width":4000,"height":3000,"created_at":"2021-10-03 16:19:09.96598 -0500 CDT m=+0.006012901"
-def findPhotos(dirname):
-    photos = []
+"""if (photo["width"] <= MaxWidth) and (photo["height"] <= MaxHeight):
+    return send_file(photo["fname"], mimetype=photo["mime"])
+else:
+    parts = photo["fname"].rsplit('.', 1)
+    fname = parts[0] + ".png"
+    with Image.open(photo["fname"]) as img:
+        new_width = MaxWidth
+        new_height = MaxHeight
+        if img.width < MaxWidth:
+            new_width = img.width
+        if img.height < MaxHeight:
+            new_height = img.height
+        imgIO = io.BytesIO()
+        img = img.resize((new_width, new_height), resample=Image.LANCZOS, box=(0,0,MaxWidth, MaxHeight))
+        img.save(imgIO, "PNG")
+        imgIO.seek(0)
+        return send_file(imgIO, mimetype='image/png', as_attachment=True, download_name=fname)"""
+
+def processPhoto(con, fname):
+    cur = con.cursor()
+    with Image.open(fname) as im:
+        width, height = im.size
+        img = (os.path.basename(fname), #"name":
+               fname, #"fname":
+               width, #"width":
+               height, #"height":
+               Image.MIME[im.format], #"mime":
+               datetime.now().isoformat() #"created_at":
+        )
+        cur.execute("insert into photowall(name, fname, width, height, mime, created_at) values(?,?,?,?,?,?)", img)
+        con.commit()
+    return True
+
+def importPhotos(dirname):
+    con = sqlite3.connect('photowall.db')
+    cur = con.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS photowall(fname PRIMARY KEY, name, width INTEGER, height INTEGER, mime, created_at)");
+
+    newPhotos = []
+
     for root, dirs, files in os.walk(dirname, topdown=False):
         for name in files:
             parts = name.rsplit('.', 1)
@@ -15,17 +52,20 @@ def findPhotos(dirname):
                 continue
             ext = parts[1]
             if ext.lower() in supported_files:
-                #wall.AddPhotoFromFile(path, time.Now())
-                with Image.open(os.path.join(root, name)) as im:
-                    width, height = im.size
-                    # XXX clean up the file path so it isnt sent to the UI
-                    img = {"name": name,
-                           "fname": os.path.join(root, name),
-                           "width": width,
-                           "height": height,
-                           "mime": Image.MIME[im.format],
-                           "created_at": datetime.now().isoformat()
-                    }
-                    #os.path.join(root, name)
-                    photos.append(img)
-    return photos
+                fname = os.path.join(root, name)
+                newPhotos.append(fname)
+
+    cur.execute("select fname from photowall")
+    for img in cur.fetchall():
+        fname = img[0]
+        if fname not in newPhotos:
+            cur.execute("delete from photowall where fname=:fname", {"fname": img[0]})
+            con.commit()
+
+    for img in newPhotos:
+        cur.execute("select count(fname) from photowall where fname=:fname", {"fname": img})
+        resp = cur.fetchone()[0]
+        if resp < 1:
+            processPhoto(con, img)
+
+    return True
